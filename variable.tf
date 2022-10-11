1937d2fd7e849de473a2c20d30b2eab60c8b5125
floreedith96@yahoo.com <floreedith96@yahoo.com>
2022-10-10 20:21:41 -0400

    feature: declare variable

--- variable.tf
+++ variable.tf
@@ -6,3 +6,38 @@ variable "aws_vpc_cidr_block" {
   type    = string
   default = "10.0.0.0/16"
 }
+
+variable "aws_vpc_tags" {
+  type = map(any)
+  default = {
+    Name = "my-igw"
+  }
+
+}
+
+variable "aws_route_table_cidr_block" {
+  type    = string
+  default = "0.0.0.0/0"
+
+}
+
+variable "aws_route_table_tags" {
+  type = map(any)
+  default = {
+    Name = "my-route"
+  }
+
+}
+
+variable "aws_sybnet_cidr_block" {
+  type    = string
+  default = "10.0.1.0/24"
+
+}
+variable "aws_subnet_tags" {
+  type = map(any)
+  default = {
+    Name = "my_subnet"
+  }
+
+}
